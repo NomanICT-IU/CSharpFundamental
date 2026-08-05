30e3f86a479caa7d05b3aeef53e2422d423a8654
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-05 10:33:59 +0600

    <feat>First Commit

---
+++ IncrementOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IncrementOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = ++a;
+            int c = 20;
+            int d = c++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
